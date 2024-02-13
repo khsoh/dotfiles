5c7b5b026155b444054ec2f9817ca13f8ba3c5ba
K H Soh <hju37823@outlook.com>
2024-02-13 17:56:02 +0800

    Updated stow command to specify home directory as target

--- dostow.zsh
+++ dostow.zsh
@@ -2,6 +2,6 @@
 
 pushd "$(dirname $0)" > /dev/null
 for d in */; do
-    stow -R ${d:s/\///} 2> >(grep -v "BUG in find_stowed_path" 1>&2)
+    stow -Rt ~ ${d:s/\///} 2> >(grep -v "BUG in find_stowed_path" 1>&2)
 done
 popd >/dev/null
