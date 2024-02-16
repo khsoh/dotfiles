3a4e0342419f7e555640314b7227bbf80d3fa5d0
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-02-16 11:34:49 +0800

    Updated files for a friendlier install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-pushd "$(dirname $0)" > /dev/null;
+pushd "$(dirname $0)" > /dev/null
 
-git pull origin main;
+git pull origin main
 
+## Will install Homebrew if not present
+(brew --version >/dev/null 2>&1) || (/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+
+# Installs packages
+brew bundle
+
+# Sets up the symlinks with stow
 for d in */; do
     stow -Rt ~ ${d/\//} 2> >(grep -v "BUG in find_stowed_path" 1>&2)
 done
 
-popd >/dev/null;
+popd >/dev/null
