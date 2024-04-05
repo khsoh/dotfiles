61ecf2c002f34e9645ec19a5e836a5d690c37455
K H Soh <hju37823@outlook.com>
2024-04-05 17:28:57 +0800

    Changed dostow.sh and unstow.sh to use only ROOT and HOME subdirectories for source

--- dostow.sh
+++ dostow.sh
@@ -3,13 +3,7 @@
 pushd "$(dirname $0)" > /dev/null
 
 # Sets up the symlinks with stow
-for d in */; do
-    srcdir=${d/\//}
-    if [[ $srcdir = "ROOT" ]]; then
-	stow -Rt / $srcdir 2> >(grep -v "BUG in find_stowed_path" 1>&2)
-    else
-	stow -Rt ~ $srcdir 2> >(grep -v "BUG in find_stowed_path" 1>&2)
-    fi
-done
+stow -Rt / ROOT 2> >(grep -v "BUG in find_stowed_path" 1>&2)
+stow -Rt ~ HOME 2> >(grep -v "BUG in find_stowed_path" 1>&2)
 
 popd >/dev/null
--- unstow.sh
+++ unstow.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 # Unstow packages
-for d in */; do
-    srcdir=${d/\//}
-    if [[ $srcdir = "ROOT" ]]; then
-	stow -Dt / $srcdir 2> >(grep -v "BUG in find_stowed_path" 1>&2)
-    else
-	stow -Dt ~ $srcdir 2> >(grep -v "BUG in find_stowed_path" 1>&2)
-    fi
-done
+stow -Dt / ROOT 2> >(grep -v "BUG in find_stowed_path" 1>&2)
+stow -Dt ~ HOME 2> >(grep -v "BUG in find_stowed_path" 1>&2)
 
