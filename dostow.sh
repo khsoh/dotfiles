6ca2ab35724dfd2464e7b92146a2f411aff8d134
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-05-18 09:39:57 +0800

    Updates to stowing ROOT to be OS-specific

--- dostow.sh
+++ dostow.sh
@@ -4,7 +4,7 @@ SCRIPTNAME=$(readlink -f ${BASH_SOURCE[0]})
 pushd "$(dirname $SCRIPTNAME)" > /dev/null
 
 # Sets up the symlinks with stow
-stow -Rt / ROOT 2> >(grep -v "BUG in find_stowed_path" 1>&2)
+[[ -d ./ROOT-$(uname) ]] && stow -Rt / ROOT-$(uname) 2> >(grep -v "BUG in find_stowed_path" 1>&2)
 stow -Rt ~ HOME-common 2> >(grep -v "BUG in find_stowed_path" 1>&2)
 
 [[ -d ./HOME-$(uname) ]] && stow -Rt ~ HOME-$(uname) 2> >(grep -v "BUG in find_stowed_path" 1>&2)
