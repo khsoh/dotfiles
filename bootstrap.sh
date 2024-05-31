a9c361abd77af11b76b6f6d874e2399a0b08ac0c
K H Soh <hju37823@outlook.com>
2024-05-31 09:43:42 +0800

    Added support for executing branches on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 declare -r SCRIPT_NAME=$(readlink -f ${BASH_SOURCE[0]})
 bootfile=$(dirname $SCRIPT_NAME)/bootstrap-$(uname).sh
 if [[ -e $bootfile ]]; then
-    source $bootfile
+    BRANCH=${BRANCH:-main} source $bootfile
 else
     echo "WARNING: bootstrap support not available for $(uname)"
 fi
