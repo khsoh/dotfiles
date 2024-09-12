444a4eea6bb9427c3a17f9489e5d46d72dee9dd2
K H Soh <hju37823@outlook.com>
2024-09-12 11:31:22 +0800

    Removed references to HOME-common

--- dostow.sh
+++ dostow.sh
@@ -5,7 +5,6 @@ pushd "$(dirname $SCRIPTNAME)" > /dev/null
 
 # Sets up the symlinks with stow
 [[ -d ./ROOT-$(uname) ]] && stow -Rt / ROOT-$(uname) 2> >(grep -v "BUG in find_stowed_path" 1>&2)
-stow -Rt ~ HOME-common 2> >(grep -v "BUG in find_stowed_path" 1>&2)
 
 [[ -d ./HOME-$(uname) ]] && stow -Rt ~ HOME-$(uname) 2> >(grep -v "BUG in find_stowed_path" 1>&2)
 
--- unstow.sh
+++ unstow.sh
@@ -2,6 +2,5 @@
 
 # Unstow packages
 stow -Dt / ROOT 2> >(grep -v "BUG in find_stowed_path" 1>&2)
-stow -Dt ~ HOME-common 2> >(grep -v "BUG in find_stowed_path" 1>&2)
 
 [[ -d ./HOME-$(uname) ]] && stow -Dt ~ HOME-$(uname) 2> >(grep -v "BUG in find_stowed_path" 1>&2)
